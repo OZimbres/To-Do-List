f03477637786c90d0f247064b1bbf47853682ab6
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-10-19 14:42:14 -0300

    Finalizado

--- src/Task.java
+++ src/Task.java
@@ -1,9 +1,10 @@
 //Classe pra guardar as tarefas
-
 public class Task {
     //Atributos
     private String descricao;
     private boolean feito;
+    private String dataCriacao;
+    private String dataConclusao;
 
     //Construtor
     public Task(String descricao) {
@@ -26,4 +27,20 @@ public class Task {
     public void setFeito(boolean feito) {
         this.feito = feito;
     }
+
+    //Get & Set dataCriacao
+    public String getDataCriacao() {
+        return dataCriacao;
+    }
+    public void setDataCriacao(String dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }   
+
+    //Get & Set dataConclusao
+    public String getDataConclusao() {
+        return dataConclusao;
+    }
+    public void setDataConclusao(String dataConclusao) {
+        this.dataConclusao = dataConclusao;
+    }   
 }
